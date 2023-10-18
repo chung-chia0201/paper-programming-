d8e2a3867ed7e94f6acd112f1e024f5dc9106d57
chung-chia0201 <handsomepig33@gmail.com>
2023-10-18 15:44:35 +0800

    test

---
+++ respeaker/detect_freq/1st_lb_ros_sub.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+## Copyright 2023 NYCU, Chung-Chia Chen
+##
+## *****************************************************
+##  DESCRIPTION :
+##  This is the subscriber(ros) on ReSpeaker Core v2.0
+##
+#
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+
+def callback(data): 
+      
+    # print the actual message in its raw format 
+    rospy.loginfo("Here's what was subscribed: %s", data.data) 
+      
+    # otherwise simply print a convenient message on the terminal 
+    print('Data from result_csm received') 
+  
+  
+def main(): 
+      
+    # initialize a node by the name 'listener'. 
+    # you may choose to name it however you like, 
+    # since you don't have to use it ahead 
+    rospy.init_node('compare_result', anonymous=True) 
+    rospy.Subscriber("result_csm", Float32MultiArray, callback) 
+      
+    # spin() simply keeps python from 
+    # exiting until this node is stopped 
+    rospy.spin() 
+  
+if __name__ == '__main__': 
+      
+    try: 
+        main() 
+    except rospy.ROSInterruptException: 
+        pass
